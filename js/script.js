23f734827fc496a7d05528642be1f2199ab08825
percevil <peeevil@gmail.com>
2019-10-23 19:49:33 -0700

    info returned

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 
 document.addEventListener('DOMContentLoaded', function(){
     
-    $('home').on('click', function() {
+    $('button').on('click', function() {
         $.ajax({
            method: 'GET',
            url: 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=iNefxWKuTjrsV6kBGllnT3cwMLAt2670'
         })
         .done(function(data) {
-            console.log(data.home);
-            // $.each(data, function(key, value) {
-            //    $('.results').append("<li>" +  value.main +  "</li>");
+            console.log(data);
+            $.each(data.results, function(key, data) {
+               $('.header').append("<li>" + data.science +  "</li>");
                    
               })
               .fail(function() {
