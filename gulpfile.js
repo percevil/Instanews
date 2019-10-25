4cee041aed4fdbc85de2f68567417aee61ccfebb
percevil <peeevil@gmail.com>
2019-10-25 15:54:54 -0700

    images work now

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,29 @@ const gulp = require("gulp"),
     terser = require("gulp-terser"),
     rename = require("gulp-rename"),
     browserSync = require("browser-sync"),
-    eslint = require("gulp-eslint");
+    eslint = require("gulp-eslint"),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    prettyError = require('gulp-prettyerror');
 
+
+
+gulp.task('sass', function () {
+    return gulp
+        .src('./sass/style.scss')
+        .pipe(prettyError())
+        .pipe(sass())
+        .pipe(
+            autoprefixer(),
+        )
+        .pipe(gulp.dest('./build/css'))
+        .pipe(cssnano())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./build/css'));
+});
+
+      
 gulp.task("lint", function () {
     return gulp
         .src("./js/*.js")
@@ -14,11 +35,13 @@ gulp.task("lint", function () {
 
 gulp.task("scripts",
     gulp.series("lint",
-        function scripts () {
+        function scripts() {
             return gulp
                 .src("./js/*.js")
                 .pipe(terser())
-                .pipe(rename({extname: ".min.js"}))
+                .pipe(rename({
+                    extname: ".min.js"
+                }))
                 .pipe(gulp.dest("./build/js"));
         }
     )
@@ -35,12 +58,14 @@ gulp.task('browser-sync', function () {
             baseDir: "./"
         }
     });
-    gulp.watch(["*.html", "build/js/*.js", "css/*.css"])
+    gulp.watch(["*.html", "build/js/*.js", "build/css/*.css"])
         .on("change", browserSync.reload);
+    
 });
 
 gulp.task("watch", function () {
     gulp.watch("js/*.js", gulp.series("scripts"));
+    gulp.watch("sass/*.scss", gulp.series("sass"));
 });
 
 gulp.task("default", gulp.parallel("browser-sync", "watch"));
--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
 
-document.addEventListener('DOMContentLoaded', function(){
-    
-    $('button').on('click', function() {
-        $.ajax({
-           method: 'GET',
-           url: 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=iNefxWKuTjrsV6kBGllnT3cwMLAt2670'
-        })
-        .done(function(data) {
-            console.log(data);
-            $.each(data.results, function(key, data) {
-               $('.header').append("<li>" + data.science +  "</li>");
-                   
-              })
-              .fail(function() {
-                $('.error-log').append('Sorry there was an error.');
-             });
-              
-        });
-     });
+  $('button').on('click', function () {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=iNefxWKuTjrsV6kBGllnT3cwMLAt2670'
+      })
+      .done(function (data) {
+        // console.log(data.results[0].abstract);
+        console.log(data);
+
+        $.each(data.results, function (key, value) {
+            // $('.site-content').append("<li>" + data.title + "</li>");
+           $('.site-content').append("<li>"+ value.abstract +"</li>");
+           $('.site-content').append(`<img src="`+value.multimedia[2].url+`"></img>`);
+
+          //INSIDE .DONE
+          })
+          //OUTSIDE .DONE
+          // .fail(function () {
+          //   $('.error-log').append('Sorry there was an error.');
+          // });
+
+      });
+  });
 
 
 
@@ -25,4 +30,4 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 
-    
+});
